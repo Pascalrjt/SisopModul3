4ddd428232ec97b686309e80de07d8744b1c99e4
Pascal Roger J T <91580977+Pascalrjt@users.noreply.github.com>
2023-05-04 09:13:17 +0700

    Update Soal4.c

--- Soal4.c
+++ Soal4.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <dirent.h>
-
 #include <pthread.h>
-
 #include <sys/stat.h>
-
 #include <unistd.h>
-
 #include <errno.h>
-
 #include <fcntl.h>
-
 #include <time.h>
 
 #define MAX_PATH 1024
