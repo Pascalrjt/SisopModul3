efe417e1532d7eddf180063b7dcdc377d688cb9c
Pascal Roger J T <91580977+Pascalrjt@users.noreply.github.com>
2023-05-06 18:14:15 +0700

    Update sisop.c

--- sisop.c
+++ sisop.c
@@ -1,49 +1,51 @@
+// Import necessary libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/shm.h>
 
+// Define the size of the matrix
 #define ROW 4
 #define COL 5
 
 // Function to calculate the factorial of a number
-int faktorial(int n) {
+int factorial(int n) {
     if (n == 0) {
         return 1;
     } else {
-        return n * faktorial(n - 1);
+        return n * factorial(n - 1);
     }
 }
 
 // Function to display the matrix
-void tampilkan_matriks(int (*hasil)[COL], char *pesan) {
-    printf("%s:\n", pesan);
+void display_matrix(int (*result)[COL], char *message) {
+    printf("%s:\n", message);
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COL; j++) {
-            printf("%d ", hasil[i][j]);
+            printf("%d ", result[i][j]);
         }
         printf("\n");
     }
 }
 
 // Function to attach to shared memory
-int (*hubungkan_shared_memory(int shmid)) [COL] {
-    int (*hasil)[COL];
+int (*attach_shared_memory(int shmid)) [COL] {
+    int (*result)[COL];
 
-    if ((hasil = shmat(shmid, NULL, 0)) == (int (*)[COL]) -1) {
+    if ((result = shmat(shmid, NULL, 0)) == (int (*)[COL]) -1) {
         perror("shmat");
         exit(1);
     }
 
-    return hasil;
+    return result;
 }
 
 // Function to detach from shared memory
-void detach_shared_memory(int (*hasil)[COL]) {
-    shmdt(hasil);
+void detach_shared_memory(int (*result)[COL]) {
+    shmdt(result);
 }
 
 // Function to delete shared memory segment
-void hapus_shared_memory(int shmid) {
+void delete_shared_memory(int shmid) {
     shmctl(shmid, IPC_RMID, NULL);
 }
 
@@ -58,26 +60,26 @@ int main() {
     }
 
     // Attach to shared memory segment
-    int (*hasil)[COL] = hubungkan_shared_memory(shmid);
+    int (*result)[COL] = attach_shared_memory(shmid);
 
     // Display the original matrix
-    tampilkan_matriks(hasil, "Matriks sebelum dihitung faktorial");
+    display_matrix(result, "Matrix before calculating factorial");
 
     // Calculate the factorial of each element in the matrix
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COL; j++) {
-            hasil[i][j] = faktorial(hasil[i][j]);
+            result[i][j] = factorial(result[i][j]);
         }
     }
 
     // Display the resulting matrix
-    tampilkan_matriks(hasil, "Matriks setelah dihitung faktorial");
+    display_matrix(result, "Matrix after calculating factorial");
 
     // Detach from shared memory
-    detach_shared_memory(hasil);
+    detach_shared_memory(result);
 
     // Delete shared memory segment
-    hapus_shared_memory(shmid);
+    delete_shared_memory(shmid);
 
     return 0;
 }
